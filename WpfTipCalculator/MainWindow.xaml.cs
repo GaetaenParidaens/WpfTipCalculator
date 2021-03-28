ffe34cb2a76d49c3eeec0000bd11d33108afa8df
Gaetaen Paridaens <73062171+GaetaenParidaens@users.noreply.github.com>
2021-03-28 16:48:29 +0200

    V1.2
    Formating

--- WpfTipCalculator/MainWindow.xaml.cs
+++ WpfTipCalculator/MainWindow.xaml.cs
@@ -1,63 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace TipCalculator.Wpf
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
         private void CalculateButton_Click(object sender, RoutedEventArgs e)
         {
-			if (tryInputIntoDouble(TextBoxAmount.Text) == true && tryInputIntoDouble(TextBoxPercentage.Text) == true )
-			{
-				double amount = Convert.ToDouble(TextBoxAmount.Text);
-				double percent = Convert.ToDouble(TextBoxPercentage.Text);
-				double totalTip = (amount / 100) * percent;
-				double totalAmount = amount - totalTip;
+            if (tryInputIntoDouble(TextBoxAmount.Text) == true && tryInputIntoDouble(TextBoxPercentage.Text) == true)
+            {
+                double amount = Convert.ToDouble(TextBoxAmount.Text);
+                double percent = Convert.ToDouble(TextBoxPercentage.Text);
+                double totalTip = (amount / 100) * percent;
+                double totalAmount = amount - totalTip;
 
-				if (percent <= 30)
-				{
-					TextBoxTipAmount.Content = totalTip;
-					TextBoxTotalAmount.Content = totalAmount;
-				}
-				else
-				{
-					MessageBox.Show("Percent is groter dan 30", "Error", MessageBoxButton.OK); 
-				}
-			}
+                if (percent <= 30)
+                {
+                    lblTipAmount.Content = totalTip;
+                    lblTotalAmount.Content = totalAmount;
+                }
+                else
+                {
+                    MessageBox.Show("The percentage must be less than or equal to 30", "Error", MessageBoxButton.OK);
+                }
+            }
         }
 
-		public bool tryInputIntoDouble(string inputAmount)
-		{
-			try
-			{
-				double tryAmount = Convert.ToDouble(inputAmount);
-				return true;
-			}
+        public bool tryInputIntoDouble(string inputAmount)
+        {
+            try
+            {
+                double tryAmount = Convert.ToDouble(inputAmount);
+                return true;
+            }
             catch
             {
-				string error = inputAmount;
-				MessageBox.Show($"Value {error} is incorrect", "error", MessageBoxButton.OK);
-				return false;
+                string error = inputAmount;
+                MessageBox.Show($"Value {error} is incorrect", "error", MessageBoxButton.OK);
+                return false;
             }
         }
     }
