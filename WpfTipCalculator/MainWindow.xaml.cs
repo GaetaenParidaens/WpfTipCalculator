fa2aff79d512cfee5dff6756f0fd75904e64c0ce
gaetaenP <gaetaen.paridaens@student.odisee.be>
2021-04-07 16:19:29 +0200

    Update

--- WpfTipCalculator/MainWindow.xaml.cs
+++ WpfTipCalculator/MainWindow.xaml.cs
@@ -1,47 +1,44 @@
 ﻿using System;
 using System.Windows;
 
-
 namespace TipCalculator.Wpf
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        
         private void CalculateButton_Click(object sender, RoutedEventArgs e)
         {
-            if (tryInputIntoDouble(TextBoxAmount.Text) == true && tryInputIntoDouble(TextBoxPercentage.Text) == true)
-            {
-                double amount = Convert.ToDouble(TextBoxAmount.Text);
-                double percent = Convert.ToDouble(TextBoxPercentage.Text);
-                double totalTip = (amount / 100) * percent;
-                double totalAmount = amount - totalTip;
+            if (!tryInputIntoDouble(TextBoxAmount.Text) || !tryInputIntoDouble(TextBoxPercentage.Text)) return;
+            var amount = Convert.ToDouble(TextBoxAmount.Text);
+            var percent = Convert.ToDouble(TextBoxPercentage.Text);
+            var totalTip = amount / 100 * percent;
+            var totalAmount = amount - totalTip;
 
-                if (percent <= 30)
-                {
-                    lblTipAmount.Content = totalTip;
-                    lblTotalAmount.Content = totalAmount;
-                }
-                else
-                {
-                    MessageBox.Show("The percentage must be less than or equal to 30", "Error", MessageBoxButton.OK);
-                }
+            if (percent <= 30)
+            {
+                LblTipAmount.Content = totalTip;
+                LblTotalAmount.Content = totalAmount;
+            }
+            else
+            {
+                MessageBox.Show("The percentage must be less than or equal to 30", "Error", MessageBoxButton.OK);
             }
         }
 
-        public bool tryInputIntoDouble(string inputAmount)
+        private bool tryInputIntoDouble(string inputAmount)
         {
             try
             {
-                double tryAmount = Convert.ToDouble(inputAmount);
+                var tryAmount = Convert.ToDouble(inputAmount);
                 return true;
             }
             catch
             {
-                string error = inputAmount;
+                var error = inputAmount;
                 MessageBox.Show($"Value {error} is incorrect", "error", MessageBoxButton.OK);
                 return false;
             }
